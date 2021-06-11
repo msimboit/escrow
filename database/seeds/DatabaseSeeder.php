3a681cd60b8d20c3542c36bdabc2797ae4064342
Msimboit <msimboit@gmail.com>
2021-06-11 14:42:06 +0300

    latest

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Database\Seeds\SupamallproductSeeder;
+use Database\Seeds\SupamalluserSeeder;
 
 class DatabaseSeeder extends Seeder
 {

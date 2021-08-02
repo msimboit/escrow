acaad6b5ebf7d712e35327005fffe115efbdf3ac
Msimboit <msimboit@gmail.com>
2021-08-02 14:40:13 +0300

    u

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,6 @@ class CreateUsersTable extends Migration
             $table->string('role')->default('vendor');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('role');
             $table->string('password')->nullable();
             $table->string('provider')->nullable();
             $table->string('provider_id')->nullable();

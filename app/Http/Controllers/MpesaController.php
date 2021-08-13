d13cd62b0807713310ea727d5eb8dbb825d55900
Msimboit <msimboit@gmail.com>
2021-08-13 15:38:40 +0300

    api

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -103,15 +103,15 @@ class MpesaController extends Controller
         $curl_post_data = [
         
         //Fill in the request parameters with valid values
-        'BusinessShortCode' => 3029009,
+        'BusinessShortCode' => 174379,
         'Password' => $this->lipaNaMpesaPassword(),
         'Timestamp' => Carbon::rawParse('now')->format('YmdHms'),
         'TransactionType' => 'CustomerPayBillOnline',
         'Amount' => 1,
         'PartyA' => $phone_number, // replace this with your phone number
-        'PartyB' => 3029009,
+        'PartyB' => 174379,
         'PhoneNumber' => $phone_number, // replace this with your phone number
-        'CallBackURL' => 'https://0efc926e5cd5.ngrok.io',
+        'CallBackURL' => 'https://supamallescrow.com/v1/escrow/transaction/confirmation',
         'AccountReference' => $phone_number,
         'TransactionDesc' => "Testing stk push on sandbox"
         ];

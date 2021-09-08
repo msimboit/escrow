fa47b2d08debe4a0c3322d42031467b6d1e36654
Msimboit <msimboit@gmail.com>
2021-09-08 12:27:16 +0300

    Mpesa

--- app/Http/Controllers/MpesaController.php
+++ app/Http/Controllers/MpesaController.php
@@ -715,8 +715,10 @@ class MpesaController extends Controller
 
                 $phone_number = substr($phone_number, -9);
                 $phone_number = 254 . $phone_number;
+
+                $trans_id = $request->orderId;
                 
-                $this->customerMpesaSTKPush($phone_number, $amount);
+                $this->customerMpesaSTKPush($phone_number, $amount, $trans_id);
                 
                 Tdetails::where('id', '=', $values['orderId'])
                         ->update(['transactioncode' => 'mpesaCode']);

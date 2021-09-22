f65258a718c71737482bb066543d6c1c220ef858
Msimboit <msimboit@gmail.com>
2021-09-22 09:29:52 +0300

    twilio

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -526,12 +526,12 @@ class TransactionController extends Controller
 
         // Your Account SID and Auth Token from twilio.com/console
         $account_sid = 'AC906dc524ed8a5bd8242c3ed1e9c5b622';
-        $auth_token = '8073b161334e57e4115dc24af0108ec1';
+        $auth_token = '000a0805027ecae3ec7d6ea57e031dbc';
         // In production, these should be environment variables. E.g.:
         // $auth_token = $_ENV["TWILIO_AUTH_TOKEN"]
 
         // A Twilio number you own with SMS capabilities
-        $twilio_number = "+19362414349";
+        $twilio_number = "+19362460202";
         $recipient = '+254700682679';
         $message = "Escrow sent this message";
 

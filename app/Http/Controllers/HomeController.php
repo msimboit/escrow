a153f6dd23f71dd24cf94ec45ee88e4c397f4afc
Msimboit <msimboit@gmail.com>
2021-09-23 13:09:10 +0300

    Ujumbe

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -49,29 +49,29 @@ class HomeController extends Controller
         $data = array('data'=> [$message_bag]);
 
         $url = 'http://ujumbesms.co.ke/api/messaging';
-        // $curl = curl_init();
-        // curl_setopt_array(
-        //     $curl,
-        //     array(
-        //             CURLOPT_URL => $url,
-        //             CURLOPT_HTTPHEADER => array('x-authorization: MDE4MzA3N2E1YjQ3YTBmZjZlOWRhMzk5ZDllM2Rk', 'email:abraham.nyabera@msimboit.net'),
-        //             CURLOPT_RETURNTRANSFER => true,
-        //             CURLOPT_POST => true,
-        //             CURLOPT_POSTFIELDS => json_encode($data)
-        //         )
-        // );
-        // $curl_response = curl_exec($curl);
-        // curl_close($curl);
-        // $res = $curl_response;
-        // dd($res);
-        // return $res;
+        $curl = curl_init();
+        curl_setopt_array(
+            $curl,
+            array(
+                    CURLOPT_URL => $url,
+                    CURLOPT_HTTPHEADER => array('x-authorization: MDE4MzA3N2E1YjQ3YTBmZjZlOWRhMzk5ZDllM2Rk', 'email:abraham.nyabera@msimboit.net'),
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_POST => true,
+                    CURLOPT_POSTFIELDS => json_encode($data)
+                )
+        );
+        $curl_response = curl_exec($curl);
+        curl_close($curl);
+        $res = $curl_response;
+        dd($res);
+        return $res;
 
-        $data = json_encode($data);
-        $response = Http::withHeaders([
-            'X-Authorization' => 'MDE4MzA3N2E1YjQ3YTBmZjZlOWRhMzk5ZDllM2Rk',
-            'Email' => 'abraham.nyabera@msimboit.net'
-        ])->post('http://ujumbesms.co.ke/api/messaging', [$data]);
+        // $data = json_encode($data);
+        // $response = Http::withHeaders([
+        //     'X-Authorization' => 'MDE4MzA3N2E1YjQ3YTBmZjZlOWRhMzk5ZDllM2Rk',
+        //     'Email' => 'abraham.nyabera@msimboit.net'
+        // ])->post('http://ujumbesms.co.ke/api/messaging', [$data]);
 
-        dd($response);
+        // dd($response);
     }
 }
